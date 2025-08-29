f38f1468f0d25beb3a5ae7c1d65a4e7a70e414cf
MiadongDeersfeet <ani.ana.yungi@gmail.com>
2025-08-29 17:49:34 +0900

    JDBC

---
+++ 00_JDBC_TEST/src/com/aclass/test/run/ReflactionRun.java
@@ -0,0 +1,21 @@
+package com.aclass.test.run;
+
+public class ReflactionRun {
+
+	public static void main(String[] args) {
+
+		try {
+			Class<?> c = Class.forName("com.aclass.test.run.SelectRun");
+			String typeName = c.getTypeName();
+			Object tr = c.newInstance();
+			System.out.println(tr);
+			System.out.println(typeName);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
+}
---
+++ Employee/src/com/homework/employee/run/SelectRun.java
@@ -0,0 +1,107 @@
+package com.homework.employee.run;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SelectRun {
+
+	public static void main(String[] args) {
+		
+		
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rset = null;
+		
+		String sql = """
+				       SELECT
+				              EMP_ID
+				            , EMP_NAME
+				            , EMP_NO
+				            , EMAIL
+				            , PHONE
+				            , DEPT_CODE
+				            , JOB_CODE
+				            , SAL_LEVEL
+				            , SALARY
+				            , BONUS
+				            , MANAGER_ID
+				            , HIRE_DATE
+					     FROM 
+					          EMPLOYEE
+					    ORDER
+					       BY HIRE_DATE     
+				""";
+		try {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		
+		conn = DriverManager.getConnection("jdbc:oracle:thin:@115.90.212.20:10000:XE",
+										   "KYK03"
+										 , "KYK031234");
+		System.out.println("DB서버 접속 성공!");
+		
+		stmt = conn.createStatement();
+		
+		rset = stmt.executeQuery(sql);
+		
+		while(rset.next()) {
+		String empId = rset.getString("EMP_ID");
+		String empName = rset.getString("EMP_NAME");
+		String empNo = rset.getString("EMP_NO");
+		String email = rset.getString("EMP_NO");
+		String phone = rset.getString("PHONE");
+		String deptCode = rset.getString("DEPT_CODE");
+		String jobCode = rset.getString("JOB_CODE");
+		String salLevel = rset.getString("SAL_LEVEL");
+		int salary = rset.getInt("SALARY");
+		double bonus = rset.getDouble("BONUS");
+		int managerId = rset.getInt("MANAGER_ID");
+		Date hireDate = rset.getDate("HIRE_DATE");
+		
+		System.out.println("사원번호: " + empId
+						+ ", 사원명: " + empName 
+					    + ", 주민번호: " + empNo 
+					    + ", 이메일: " + email
+					    + ", 연락처: " + phone
+					    + ", 부서코드: " + deptCode
+					    + ", 직급코드: " + jobCode
+					    + ", 급여등급: " + salLevel
+					    + ", 급여: " + salary
+					    + ", 보너스율: " + bonus
+					    + ", 관리자사번: " + managerId
+					    + ", 입사일: " + hireDate);
+		}		
+		
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rset != null && !rset.isClosed())
+				rset.close();
+			} catch(SQLException e) {
+				e.printStackTrace();
+			} try {
+				if(stmt != null && !stmt.isClosed())
+					stmt.close();
+			} catch(SQLException e) {
+				e.printStackTrace();
+			} try {
+				if(conn != null && !conn.isClosed())
+					conn.close();
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		
+		
+	}
+
+}
